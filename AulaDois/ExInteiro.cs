f920564fc31963a7be61af71e7f2a31c718392e5
professorthiagoxavier <professorthiagoxavier@gmail.com>
2020-03-13 21:49:18 -0300

    aula 2

---
+++ AulaDois/ExInteiro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AulaDois
+{
+    public partial class ExInteiro : Form
+    {
+        public ExInteiro()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {                   //converter para short
+            short idade = short.Parse(txtIdade.Text);
+            int cep = int.Parse(txtCEP.Text);
+            long telefone = long.Parse(txtTelefone.Text);
+
+            string mensagem = "Idade: " + idade;
+            mensagem += "Cep: " + cep;
+            mensagem += "Telefone: " + telefone;
+
+            MessageBox.Show(mensagem);
+
+        }
+    }
+}
---
+++ AulaDois/Salario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AulaDois
+{
+    public partial class Salario : Form
+    {
+        public Salario()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double salario = double.Parse(txtSalario.Text);
+            float desconto = float.Parse(txtDesconto.Text);
+            double calDesconto = salario * desconto / 100; //desconto
+            salario = salario - calDesconto;
+            MessageBox.Show("Salário do cabra: " + Math.Round(salario, 2));
+        }
+    }
+}
